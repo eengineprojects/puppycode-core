3391e1ae1c7433d603243a8592ab9d414e74fd54
prettycrunchyguy <prettycrunchyguy>
2023-10-09 12:01:14 -0500

    fix padding

--- editor/script.js
+++ editor/script.js
@@ -4,7 +4,10 @@ function rename(){
 window.onload = function(){
     //draggable = document.getElementsByClassName("puppycode-block-drag");
     editor = document.getElementById('container');
-    setTimeout(function(){document.getElementsByClassName("mdl-layout__drawer-button")[0].remove()},250)
+    setTimeout(function(){
+        document.getElementsByClassName("mdl-layout__drawer-button")[0].remove()
+        document.getElementsByClassName("mdl-layout__tab-bar-button mdl-layout__tab-bar-left-button")[0].remove()
+    },250)
 }
 function newBlock(){
     for (i = 0; i < document.getElementsByClassName("puppycode-block-drag").length; i++){
