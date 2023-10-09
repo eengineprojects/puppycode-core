39eaa342b3fc218a8dd3beef4de60163ec6acc1e
prettycrunchyguy <prettycrunchyguy>
2023-10-08 22:15:02 -0500

    ui keep changing

--- editor/script.js
+++ editor/script.js
@@ -4,6 +4,7 @@ function rename(){
 window.onload = function(){
     //draggable = document.getElementsByClassName("puppycode-block-drag");
     editor = document.getElementById('container');
+    setTimeout(function(){document.getElementsByClassName("mdl-layout__drawer-button")[0].remove()},250)
 }
 function newBlock(){
     for (i = 0; i < document.getElementsByClassName("puppycode-block-drag").length; i++){
