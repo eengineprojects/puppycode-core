51c72cff1d33ba76345b579a1e064419848bf8e0
prettycrunchyguy <prettycrunchyguy>
2023-10-15 14:30:42 -0500

    run button

--- editor/runtime.js
+++ editor/runtime.js
@@ -1,14 +1,18 @@
 const play_button = document.querySelector("#play");
 const Console = document.querySelector("#console");
 const runtime = document.querySelector("#consoleRuntime");
-const IDE = document.getElementById("container");
+//const IDE = document.getElementById("container");
+const editor = document.getElementById("container")
 
 function openConsole() {
     IDE.style.display = "none";
 }
 
 function run() {
-    
+    content = IDE.getValue();
+    content = content.replaceAll("\n",";")
+    content = content.replaceAll("\t","")
+    eval(content);
 }
 
-play_button.addEventListener("click", openConsole);
+//play_button.addEventListener("click", openConsole);
--- editor/script.js
+++ editor/script.js
@@ -3,7 +3,7 @@ function rename() {
 }
 window.onload = function () {
     //draggable = document.getElementsByClassName("puppycode-block-drag");
-    editor = document.getElementById('container');
+    //editor = document.getElementById('container');
     setTimeout(function () {
         document.getElementsByClassName("mdl-layout__drawer-button")[0].remove()
         document.getElementsByClassName("mdl-layout__tab-bar-button mdl-layout__tab-bar-left-button")[0].remove()
